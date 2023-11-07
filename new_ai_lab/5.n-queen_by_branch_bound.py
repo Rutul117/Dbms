bb15f037f5398172457d1eb02ce581c8a20587b7
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2023-11-07 20:42:16 +0530

    Update 5.n-queen_by_branch_bound.py

--- new_ai_lab/5.n-queen_by_branch_bound.py
+++ new_ai_lab/5.n-queen_by_branch_bound.py
@@ -1,16 +1,24 @@
+import heapq
+
+class Node:
+    def __init__(self, board, row, col):
+        self.board = board  # The current state of the board
+        self.row = row      # The row where a queen is placed
+        self.col = col      # The column where a queen is placed
+
+    def __lt__(self, other):
+        return False
+
 def is_safe(board, row, col):
     # Check if a queen can be placed in a given cell
-    # Check row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on the left side
     for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -18,27 +26,31 @@ def is_safe(board, row, col):
     return True
 
 def solve_n_queens(n):
-    def solve(board, col):
-        if col >= n:
-            return True
+    board = [[0] * n for _ in range(n)]
+    min_col = [n] * n
+    min_row = [n] * n
 
-        for i in range(n):
-            if is_safe(board, i, col):
-                board[i][col] = 1
-                if solve(board, col + 1):
-                    return True
-                board[i][col] = 0
+    root = Node(board, 0, -1)
+    heap = [(root, 0)]
 
-        return False
+    while heap:
+        current, col = heapq.heappop(heap)
+        col += 1
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    if not solve(board, 0):
-        print("No solution exists.")
-        return
-
-    for row in board:
-        print(' '.join('Q' if cell == 1 else '.' for cell in row))
+        for row in range(n):
+            if is_safe(current.board, row, col):
+                new_board = [row[:] for row in current.board]
+                new_board[row][col] = 1
+                new_node = Node(new_board, row, col)
+                if col == n - 1:
+                    return new_node.board
+                heapq.heappush(heap, (new_node, col))
 
 if __name__ == "__main__":
     n = 8  # Change this to the desired board size
-    solve_n_queens(n)
+    solution = solve_n_queens(n)
+    if solution:
+        for row in solution:
+            print(' '.join('Q' if cell == 1 else '.' for cell in row))
+    else:
+        print("No solution exists.")
