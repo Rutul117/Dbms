2d3b1ca11861e974156617a15be25cd4d0c9c33c
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2023-11-07 21:42:13 +0530

    Update 2a copy.sql

--- dbms_lab_copy/2a copy.sql
+++ dbms_lab_copy/2a copy.sql
@@ -1,22 +1,9 @@
--- Create the Student Database
-CREATE DATABASE IF NOT EXISTS StudentDB;
-USE StudentDB;
-
--- Create the EMP table
+- Create the EMP table
 CREATE TABLE EMP (
     Emp_no INT PRIMARY KEY,
     Emp_name VARCHAR(255),
     Dept_no INT
 );
-
--- Insert data into the EMP table
-INSERT INTO EMP (Emp_no, Emp_name, Dept_no)
-VALUES
-    (1, 'Ram', 101),
-    (2, 'Shyam', 102),
-    (3, 'Sita', 101),
-    (4, 'Gita', 103),
-    (5, 'Hari', 103);
 
 -- Create the DEPT table
 CREATE TABLE DEPT (
@@ -24,49 +11,48 @@ CREATE TABLE DEPT (
     Dept_name VARCHAR(255)
 );
 
--- Insert data into the DEPT table
-INSERT INTO DEPT (Dept_no, Dept_name)
-VALUES
-    (101, 'HR'),
-    (102, 'Finance'),
-    (103, 'IT');
-
 -- Create the Address table
 CREATE TABLE Address (
-    Dept_name VARCHAR(255),
+    Dept_name INT PRIMARY KEY,
     Dept_location VARCHAR(255)
 );
 
+-- Insert data into the EMP table
+INSERT INTO EMP (Emp_no, Emp_name, Dept_no) VALUES
+(1, 'John Doe', 101),
+(2, 'Jane Smith', 102),
+(3, 'Bob Johnson', 101),
+(4, 'Alice Brown', 103);
+
+-- Insert data into the DEPT table
+INSERT INTO DEPT (Dept_no, Dept_name) VALUES
+(101, 'Human Resources'),
+(102, 'Marketing'),
+(103, 'Finance');
+
 -- Insert data into the Address table
-INSERT INTO Address (Dept_name, Dept_location)
-VALUES
-    ('HR', 'Location1'),
-    ('Finance', 'Location2'),
-    ('IT', 'Location3');
+INSERT INTO Address (Dept_no, Dept_location) VALUES
+(101, 'HR Building, 5th Avenue'),
+(102, 'Marketing Tower, Main Street'),
+(103, 'Finance Plaza, Park Avenue');
 
--- Q1. Display the location of the department where employee 'Ram' is working.
+
+-- Q1. Display the location of departunent where employee Ram is working.
 SELECT A.Dept_location
-FROM Address A
-JOIN DEPT D ON A.Dept_name = D.Dept_name
-JOIN EMP E ON D.Dept_no = E.Dept_no
+FROM EMP E
+JOIN DEPT D ON E.Dept_no = D.Dept_no
+JOIN Address A ON D.Dept_no = A.Dept_name
 WHERE E.Emp_name = 'Ram';
-
--- Q2. Create a view to store the total number of employees working in each department in ascending order.
-CREATE OR REPLACE VIEW EmployeeCount AS
-SELECT D.Dept_name, COUNT(E.Emp_no) AS TotalEmployees
+-- Q2. Create view to store total no of employees working in each department in
+ascending ordeR
+CREATE VIEW DepartmentEmployeeCount AS
+SELECT D.Dept_name, COUNT(E.Emp_no) AS EmployeeCount
 FROM DEPT D
 LEFT JOIN EMP E ON D.Dept_no = E.Dept_no
 GROUP BY D.Dept_name
-ORDER BY TotalEmployees ASC;
-
--- Q3. Find the name of the department in which no employee is working.
+ORDER BY EmployeeCount ASC;
+ -- 03. Find the name of department in which no employce is working
 SELECT D.Dept_name
 FROM DEPT D
 LEFT JOIN EMP E ON D.Dept_no = E.Dept_no
 WHERE E.Emp_no IS NULL;
-
--- Select all records from EMP table
-SELECT * FROM EMP;
-
--- Drop the StudentDB database
--- DROP DATABASE StudentDB;
