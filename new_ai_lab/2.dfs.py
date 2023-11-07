9ebbce2d942b815e3e1a6998d3ea183f735a59c1
Rutul D. Bhosale <68531667+Rutul117@users.noreply.github.com>
2023-11-07 17:09:05 +0530

    Update 2.dfs.py

--- new_ai_lab/2.dfs.py
+++ new_ai_lab/2.dfs.py
@@ -1,17 +1,5 @@
 from collections import deque
 
-# Define a graph using an adjacency list.
-graph = {
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'E'],
-    'C': ['A', 'F'],
-    'D': ['B'],
-    'E': ['B', 'F'],
-    'F': ['C', 'E']
-}
-
-def dfs(graph, start):from collections import deque
-
 def dfs(graph, start):
     visited = set()  # Keep track of visited nodes
     stack = [start]  # Use a stack for DFS
@@ -43,19 +31,3 @@ if start_node in graph:
     dfs(graph, start_node)
 else:
     print("Start node not found in the graph.")
-
-    visited = set()  # Keep track of visited nodes
-    stack = [start]  # Use a stack for DFS
-
-    print("DFS Traversal:")
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            print(node, end=' ')
-            visited.add(node)
-            # Push unvisited neighbors onto the stack in reverse order.
-            for neighbor in reversed(graph[node]):
-                if neighbor not in visited:
-                    stack.append(neighbor)
-
-dfs(graph,'A')
